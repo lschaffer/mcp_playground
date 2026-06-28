02f4a98f4fe76e2336ccec8da22960f912ffef49
laszloschaffer <laszloschaffer@gmail.com>
2026-06-28 14:22:41 +0200

    Replace flutter_markdown with flutter_markdown_plus, revert openai_dart to compatible ^0.6.2

--- lib/widgets/chat_bubble.dart
+++ lib/widgets/chat_bubble.dart
@@ -3,7 +3,7 @@ import 'dart:io' as io;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:file_picker/file_picker.dart';
 import '../models.dart';
 import '../playground_controller.dart';
